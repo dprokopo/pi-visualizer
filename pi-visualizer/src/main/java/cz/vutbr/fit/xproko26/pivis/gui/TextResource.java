f8b02d53e4c4e7b152dcc2aa51abfc5aa453f419
Dagmar Prokopova <xproko26@stud.fit.vutbr.cz>
2017-05-24 12:08:34 +0200

    Fixed help typos and errors.

--- pi-visualizer/src/main/java/cz/vutbr/fit/xproko26/pivis/gui/TextResource.java
+++ pi-visualizer/src/main/java/cz/vutbr/fit/xproko26/pivis/gui/TextResource.java
@@ -46,7 +46,7 @@ public class TextResource extends ResourceBundle {
         switch(key) {
             case "consolehelp":
                 return  "Use one of the following commands to execute the specified action. \n"
-                        + "More details on how to use this application can be found at help menu.\n"
+                        + "More details on how to use this application can be found in help menu.\n"
                         + "----------------------------------------------------------------\n"
                         + "command\t\t\t action\n"
                         + "----------------------------------------------------------------\n"
@@ -67,14 +67,14 @@ public class TextResource extends ResourceBundle {
             case "intro-html":             
                 return "<html>"
                     + "<h1>Introduction</h1>"
-                    + "<p>The aim of this application is to aid at understanding and analyzing the pi-calculus expressions by providing a possibility to view and interact with its graphical representation. It is, however, recommended that the users of this applications have at least basic theoretical knowledge of the pi-calculus.</p>"
+                    + "<p>The aim of this application is to aid understanding and analyzing the pi-calculus expressions by providing a possibility to view and interact with its graphical representation. It is, however, recommended that the users of this applications have at least basic theoretical knowledge of pi-calculus.</p>"
                     + "<br>"                       
-                    + "<p>The core functionallity of the application is described in the individual chapters of this help with refference to the following components provided by the application interface:</p>"
+                    + "<p>The core functionallity of the application is described in individual chapters of this help with reference to the following components provided by the application interface:</p>"
                     + "<ul>"
                     + "<li><b>MenuBar</b> comprised of 4 main menus - File, Control, Settings, Help</li>"
-                    + "<li><b>ToolBar</b> for faster access to key items from menus</li>"
-                    + "<li><b>Console</b> for text input and control (first tab on the top)</li>"
-                    + "<li><b>Environment panel/editor</b> (second tab on the top)</li>"
+                    + "<li><b>ToolBar</b> for faster access to key menu items</li>"
+                    + "<li><b>Console</b> for text input and control (first tab)</li>"
+                    + "<li><b>Environment panel/editor</b> (second tab)</li>"
                     + "<li><b>Graph canvas</b> showing graphical representation of the processed expression (at the center)</li>"
                     + "<li><b>Text line</b> showing textual representation of the processed expression (at the bottom)</li>"
                     + "<li><b>Reduction panel/list</b> (initially hidden)</li>"
@@ -90,7 +90,7 @@ public class TextResource extends ResourceBundle {
                     + "<br>"    
                     + "<p>&nbsp <b>agent</b> <FONT COLOR=RED>Id</FONT>(<FONT COLOR=BLUE>free names</FONT>) = <FONT COLOR=GREEN>expression</FONT></p>"
                     + "<br>"
-                    + "<p>Every process <FONT COLOR=RED>identifier</FONT> starts with an uppercase letter, while every <FONT COLOR=BLUE>name</FONT> starts with a lowercase letter. Multiple names can be ordered into the list by using comma as a separator. The following grammar describes the syntax of an <FONT COLOR=GREEN>expression</FONT>:</p>"
+                    + "<p>Every process <FONT COLOR=RED>identifier</FONT> starts with an uppercase letter, while every <FONT COLOR=BLUE>name</FONT> starts with a lowercase letter. Multiple names are separated by comma. The following grammar describes the syntax of an <FONT COLOR=GREEN>expression</FONT>:</p>"
                     + "<br>"
                     + "<table>"
                         
@@ -148,9 +148,9 @@ public class TextResource extends ResourceBundle {
                     + "<br>"
                     + "<p>Parentheses can be used in expressions in order to resolve ambiguity or change the order of evaluation.</p>"
                     + "<br>"
-                    + "<p>Once the process is specified, its definition can be print out to the console using the command <b>env <FONT COLOR=RED>Id</FONT></b>. In case that the key word <b>env</b> is used withou process identifier, all process definitions are printed.</p>"
+                    + "<p>Once the process is specified, its definition can be printed out to the console using the command <b>env <FONT COLOR=RED>Id</FONT></b>. In case that the key word <b>env</b> is used without process identifier, all process definitions are printed.</p>"
                     + "<br>"
-                    + "<p>Another way how to add, modify or remove process definitions is to use the <b>Environment editor</b>. The syntax of process definition used in the editor is the same as in console. Remember that all changes made in the Environment editor needs to be commited in order to apply.</p>"
+                    + "<p>Another way to add, modify or remove process definitions is to use the <b>Environment editor</b>. The syntax of process definition used in the editor is the same as in console. Remember that all changes made in the Environment editor must be commited in order to apply.</p>"
                     + "<br>"    
                     + "<p>Process definitions can be also imported from a text file by selecting a <b>Import environment</b> item from File menu or using the appropriate button in the Environment panel.</p>"
                     + "<br>"
@@ -166,9 +166,9 @@ public class TextResource extends ResourceBundle {
                     + "<br>"
                     + "<p>Syntax of expression is described in chapter 'Process specification'.</p>"
                     + "<br>"
-                    + "<p>Another option how to visualize expression is via the dialog window which can be opened from Control menu by selecting item <b>Visualize expression</b> or using keyboard shortcut <b>F4</b>.</p>"
+                    + "<p>Another way to visualize expression is via the dialog window which can be opened from Control menu by selecting item <b>Visualize expression</b> or using keyboard shortcut <b>F4</b>.</p>"
                     + "<br>"    
-                    + "<p>In case that the specified expression contains concretization of process which was not defined in advance, the warning will be shown in the console. However, it is possible to define the process additionaly.</p>"
+                    + "<p>In case that the specified expression contains concretization of process which was not defined in advance, warning will be shown in the console. However, it is possible to define the process later.</p>"
                     + "<br>"    
                     + "<p>The expression is visualized in a graphical form on the graph canvas as well as textual form on the bottom text line.</p>"
                     + "</html>";
@@ -194,7 +194,7 @@ public class TextResource extends ResourceBundle {
             case "reduction-html":                
                 return "<html>"
                     + "<h1>Reduction</h1>"
-                    + "<p>In order to execute reduction upon the visualized expression, one must first select appropriate nodes for reduction. This can be done eigher manually by selecting nodes in the graph as described in chapter 'Graph manipulation' or by selecting reduction from the Reduction list. Reduction list can be made visible or hidden from Settings menu (<b>Reduction list</b> item).<p>"
+                    + "<p>In order to perform reduction on the visualized expression, one must first select appropriate nodes for reduction. This can be done either manually by selecting nodes in the graph as described in chapter 'Graph manipulation' or by selecting reduction from the Reduction list. Reduction list can be made visible or hidden from the Settings menu (<b>Reduction list</b> item).<p>"
                     + "<br>"
                     + "<p>The reduction itself can be then executed using the <b>reduce</b> command in the console or by selecting <b>Reduce expression</b> item from Control menu or by using keyboard shortcut <b>F5</b>.</p>"
                     + "<br>"
@@ -208,7 +208,7 @@ public class TextResource extends ResourceBundle {
                     + "<h1>Simplification</h1>"
                     + "<p>After performing several reductions or creating multiple replication branches, the visualized expression might contains duplicities or unnecessary nodes. For this case it might be useful to simplify the expression by writing either <b>simplify</b> command into the console or by selecting <b>Simplify expression</b> from Control menu or by using keyboard shortcut <b>F6</b>.<p>"
                     + "<br>"
-                    + "<p>The effect of simplification is the following: </p>"
+                    + "<p>The simplification has following effects: </p>"
                     + "<ul>"
                     + "<li>The non-active nodes followed by Nil expressions are removed</li>"
                     + "<li>The Nil branches of summations and parallel compositions are removed</li>"
@@ -224,11 +224,11 @@ public class TextResource extends ResourceBundle {
                 return "<html>"
                     + "<h1>File management</h1>"
                     + "<p>There are several options for generating output from the application.<p>"
-                    + "<p>The whole data model of the application (including all process definitions, visualized expression and selections) can be saved at once using the <b>Save</b> or <b>Save as</b> item from the File menu or by using corresponding keyboard shortcuts <b>Ctrl-S</b> or <b>Ctrl-Sift-S</b>. This action will generate a 'pvs' file which can be later used to load the saved state of the application by using <b>Open</b> item in File menu or keyboard shortcut <b>Ctrl-O</b>.</p>"
+                    + "<p>The whole data model of the application (including all process definitions, visualized expression and selections) can be saved at once using the <b>Save</b> or <b>Save as</b> item from the File menu or by using corresponding keyboard shortcuts <b>Ctrl-S</b> or <b>Ctrl-Sift-S</b>. This action will generate a PVS file (*.pvs) which can be later used to load the saved state of the application by using <b>Open</b> item in File menu or keyboard shortcut <b>Ctrl-O</b>.</p>"
                     + "<br>"    
                     + "<p>Another option is to export only the environment (i.e. process definitions) by using item <b>Export environment</b> in the File menu. This will generate a human-readable text file containing all process definitions. Such file can be easily modified or even created from scratch in any text editor and used later to import all process definitions back into the application with the usage of <b>Import environment</b> item in the File menu.</p>"
                     + "<br>"
-                    + "<p>The last (,but not least usefull) way how to generate output from the application is to export the graphical representation of the expression into one of the raster or vector formats offered by the <b>Export graph as</b> submenu in the File menu. The whole graph is always exported no matter if it is only partially visible on the canvas. The size of the exported graphic can be adjusted by zooming in or out with the mouse wheel.<p/>"
+                    + "<p>The last (but not the least useful) way how to generate output from the application is to export the graphical representation of the expression into one of the raster or vector formats offered by the <b>Export graph as</b> submenu in the File menu. The whole graph is always exported regardless of whether it is fully visible on the canvas. The exported image size can be adjusted by zooming in or out with the mouse wheel.<p/>"
                     + "</html>"; 
                 
             case "about-html":
@@ -238,7 +238,7 @@ public class TextResource extends ResourceBundle {
                     + "<br>"
                     + "<p>This software was developed as Master thesis project at Brno University of Technology, Czech Republic by Dagmar Prokopova (xproko26@stud.fit.vutbr.cz).</p>"
                     + "<br>"    
-                    + "<p>It is available as an open source under Apache 2.0 license at:</p>"
+                    + "<p>It is available under Apache 2.0 open source license:</p>"
                     + "<br>"
                     + "</html>";
                 
